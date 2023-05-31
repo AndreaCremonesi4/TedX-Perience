864dfa122faa7293421028421ff9fd48026bd304
Ombuman <73274978+Ombuman@users.noreply.github.com>
2023-05-31 19:17:53 +0200

    Update handler.js

--- Colombu - Odajiu/Lambda Functions/Most_Liked/handler.js
+++ Colombu - Odajiu/Lambda Functions/Most_Liked/handler.js
@@ -8,30 +8,28 @@ module.exports.Most_Liked = (event, context, callback) => {
     if (event.body) {
         body = JSON.parse(event.body)
     }
-   
+
    if(body.num_video==null) {
        body.num_video = 10;
    }
    if(body.num_video < 1) {
        body.num_video = 1;
    }
-   
+
     connect_to_db().then(async () => {
 
         try{
             let most_liked_list = [];
-            let talk_list = await talksModel.find({},{_id:0, title: 1,url:1, likes:1}).sort({likes:-1}).limit(body.num_video); 
-            
-             talk_list.forEach((talk) =>{
+            let talk_list = await talksModel.find({},{_id:0, title: 1, url:1, likes:1}).sort({likes:-1}).limit(body.num_video);
+
+            talk_list.forEach((talk) =>{
                 most_liked_list.push({
                     Title: talk.title, 
                     URL: talk.url,
                     Likes: talk.likes
-                    
-                });            
-                 
-             });
-         
+                });
+            });
+
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(most_liked_list)
