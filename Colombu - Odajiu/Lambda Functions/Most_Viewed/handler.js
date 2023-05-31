ed683298eb985becd2728e3b99be168b38a979dd
Ombuman <73274978+Ombuman@users.noreply.github.com>
2023-05-31 19:17:07 +0200

    Update handler.js

--- Colombu - Odajiu/Lambda Functions/Most_Viewed/handler.js
+++ Colombu - Odajiu/Lambda Functions/Most_Viewed/handler.js
@@ -1,5 +1,4 @@
 const connect_to_db = require('./db');
-
 const talksModel = require('./Talk');
 
 module.exports.Most_Viewed = (event, context, callback) => {
@@ -9,30 +8,28 @@ module.exports.Most_Viewed = (event, context, callback) => {
     if (event.body) {
         body = JSON.parse(event.body)
     }
-   
+
    if(body.num_video==null) {
        body.num_video = 10;
    }
    if(body.num_video < 1) {
        body.num_video = 1;
    }
-   
+
     connect_to_db().then(async () => {
 
         try{
             let most_viewed_list = [];
-            let talk_list = await talksModel.find({},{_id:0, title: 1,url:1, num_views:1}).sort({avg_views:-1}).limit(body.num_video); 
-            
-             talk_list.forEach((talk) =>{
+            let talk_list = await talksModel.find({},{_id:0, title: 1,url:1, avg_views:1}).sort({avg_views:-1}).limit(body.num_video);
+
+            talk_list.forEach((talk) =>{
                 most_viewed_list.push({
-                    title: talk.title, 
+                    Title: talk.title, 
                     URL: talk.url,
-                    num_views: talk.num_views
-                    
-                });            
-                 
-             });
-         
+                    Average_Views: talk.avg_views
+                });
+            });
+
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(most_viewed_list)
