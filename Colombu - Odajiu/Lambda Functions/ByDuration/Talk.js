9bf6d77394bd7f64b0051c6e8ab52ac960556222
RomeoOdajiu <73280054+RomeoOdajiu@users.noreply.github.com>
2023-06-09 19:09:10 +0200

    Update Talk.js

--- Colombu - Odajiu/Lambda Functions/ByDuration/Talk.js
+++ Colombu - Odajiu/Lambda Functions/ByDuration/Talk.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose');
 
+const talk_schema = new mongoose.Schema({
+_id: String,
+title: String,
+author: String,
+date: String,
+views: Number,
+likes: Number,
+url: String,
+main_speaker: String,
+details: String,
+posted: String,
+num_views: Number,
+duration: String,
+tags: Object,
+watch_next_list: Object,
+avg_views: Number
+}, { collection: 'tedx_data' });
+
+module.exports = mongoose.model('talk', talk_schema);
